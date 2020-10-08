9ecb46afb6674d87ab64c6363e21f6b58c2bf6e9
Mauro <molinamauro2@gmail.com>
2020-10-08 12:38:42 -0300

    added next meets in home page

--- controllers/home.js
+++ controllers/home.js
@@ -1,13 +1,46 @@
 const Category = require('../models/Category');
+const Meet = require('../models/Meet');
+const Group = require('../models/Group');
+const moment = require('moment');
+const Sequelize = require('sequelize');
+const User = require('../models/User');
+const Op = Sequelize.Op;
 
 exports.home = async (req, res) => {
     const queries = [];
     queries.push(
         await Category.findAll({})
     );
-    const [ categories ] = await Promise.all(queries);
+    queries.push(
+        await Meet.findAll({
+            attributes: ['slug', 'title', 'date', 'time'],
+            where: {
+                date: {
+                    [Op.gte]: moment(new Date()).format("YYYY-MM-DD")
+                }
+            },
+            order: [
+                ['date', 'ASC'],
+                ['time', 'ASC']
+            ],
+            limit: 3,
+            include: [
+                {
+                    model: Group,
+                    attributes: ['image']
+                },
+                {
+                    model: User,
+                    attributes: ['name', 'image']
+                }
+            ]
+        })
+    );
+    const [ categories, meets ] = await Promise.all(queries);
     res.render('home', {
         pageName: 'Inicio',
-        categories
+        categories,
+        meets,
+        moment
     });
 };
